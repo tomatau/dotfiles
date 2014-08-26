33265a3d1309d06684b381d13ef53daccef4dc15
tomatau <toma_toom@hotmail.com>
2014-08-26 13:27:07 +0100

    trimmed the test script

--- tmp/test.sh
+++ tmp/test.sh
@@ -1,12 +1,1 @@
-# ~/.extra, ~/.bash_prompt, ~/.exports, ~/.aliases and ~/.functions
-# ~/.extra can be used for settings you don’t want to commit
-# for file in ./copy/.shell/{extra,bash_prompt,exports,aliases,functions}; do
-#   [ -r ${file} ] && echo ${file}
-# done
-
-for file in ./copy/.shell/*; do
-  # don't need checks! :o
-  echo "$file"
-done
-
-unset file
+# Whilst building scripts, use this file for convenient testin
