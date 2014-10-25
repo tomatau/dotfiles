511c43c4e3fb0272ee8d2e056142772af658c9ea
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-10-25 15:57:02 +0100

    fixed version inits

--- copy/.shell/inits.sh
+++ copy/.shell/inits.sh
@@ -2,12 +2,12 @@
 
 [[ -s ~/.bin/z/z.sh ]] && . ~/.bin/z/z.sh
 
-if [[ which rbenv > /dev/null ]]; then
-  eval "$(rbenv init -)"
+if which rbenv > /dev/null; then
+  eval "$(rbenv init -)";
 fi
 
-if [[ which pyenv > /dev/null ]]; then
-  eval "$(pyenv init -)"
+if which pyenv > /dev/null; then
+  eval "$(pyenv init -)";
 fi
 
 PHPBREW_SET_PROMPT=1
