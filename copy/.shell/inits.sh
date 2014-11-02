817bc0dcab4673d55c6636bf173c14a4ab06ad6d
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-11-02 23:11:37 +0000

    updated path troubles

--- copy/.shell/inits.sh
+++ copy/.shell/inits.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+PATH="$HOME/.rbenv/bin:$PATH"
+
+PYENV_ROOT="$HOME/.pyenv"
+PATH="$PYENV_ROOT/bin:$PATH"
+
+# APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
+# PATH=/usr/local/bin:$(path_remove /usr/local/bin)
+
+# node
+# export NODE_PATH='/usr/local/lib/node_modules'
+# export PATH="usr/local/lib/node_modules:$PATH"
+
+# ruby
+# export RBENV_ROOT=/usr/local/var/rbenv
+
+export PATH
+
 [[ -s ~/.bin/z/z.sh ]] && . ~/.bin/z/z.sh
 
 if which rbenv > /dev/null ; then
