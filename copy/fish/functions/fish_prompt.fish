8c2e2b57672d7a90c2c11234d5249a3dd43d62b8
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2017-12-18 18:57:19 +0000

    added status to fish prompt

--- copy/fish/functions/fish_prompt.fish
+++ copy/fish/functions/fish_prompt.fish
@@ -1,8 +1,8 @@
-function __prompt_parent -a dir
+function __prompt_parent -a dir -d 'prompt_pwd without current dir'
   string replace $dir '' (prompt_pwd)
 end
 
-function __prompt_dir
+function __prompt_dir -d 'Show dir with current dir bolded'
   set -l dir (basename $PWD)
   set -l set_amber (set_color e69900)
   set -l set_yellow_bold (set_color -o FFCC66)
@@ -47,7 +47,6 @@ function __start_segment -S -d 'Start a prompt segment'
   set current_bg $bg
 end
 
-
 function __finish_segments -S -d 'Close open prompt segments'
   set -l right_arrow ▶
   set -l left_arrow ◀
@@ -62,11 +61,79 @@ function __finish_segments -S -d 'Close open prompt segments'
   set current_bg
 end
 
-function __prompt_node_version
-  echo -ns '⬢ ' (nvm current)
+function __prompt_node_version -d 'Show current node version'
+  echo -ns '⏣ ' (nvm current)
+end
+
+function __bobthefish_prompt_status -S -a last_status \
+  -d 'Display symbols for a non zero exit status, root and background jobs'
+  set -l nonzero
+  set -l superuser
+  set -l bg_jobs
+  set -l cant_write
+  set -l nonzero_exit_glyph '! '
+  set -l superuser_glyph '$ '
+  set -l bg_job_glyph '% '
+  set -l cant_write_glyph 'ǂ '
+  set -l colour_initial_segment_exit ffffcc ce000f --bold
+  set -l colour_initial_segment_cant ffffcc ce000f --bold
+  set -l colour_initial_segment_su ffffcc 189303 --bold
+  set -l colour_initial_segment_jobs ffffcc 255e87 --bold
+
+  # Last exit was nonzero
+  [ $last_status -ne 0 ]
+    and set nonzero $nonzero_exit_glyph
+
+  # if superuser (uid == 0)
+  [ (id -u) -eq 0 ]
+    and set superuser $superuser_glyph
+
+  # Jobs display
+  [ (jobs -l | wc -l) -gt 0 ]
+    and set bg_jobs $bg_job_glyph
+
+  not [ -w "$PWD" ]
+    and set cant_write $cant_write_glyph
+
+  if [ "$nonzero" -o "$superuser" -o "$bg_jobs" -o "$cant_write" ]
+    __start_segment $colour_initial_segment_exit
+    if [ "$nonzero" ]
+      set_color normal
+      set_color -b $colour_initial_segment_exit
+      if [ "$theme_show_exit_status" = 'yes' ]
+        echo -ns $last_status ' '
+      else
+        echo -n $nonzero_exit_glyph
+      end
+    end
+
+    if [ "$superuser" ]
+      set_color normal
+      if [ -z "$FAKEROOTKEY" ]
+        set_color -b $colour_initial_segment_su
+      else
+        set_color -b $colour_initial_segment_exit
+      end
+
+      echo -n $superuser_glyph
+    end
+
+    if [ "$bg_jobs" ]
+      set_color normal
+      set_color -b $colour_initial_segment_jobs
+      echo -n $bg_job_glyph
+    end
+
+    if [ "$cant_write" ]
+      set_color normal
+      set_color -b $colour_initial_segment_cant
+      echo -n $cant_write_glyph
+    end
+  end
 end
 
 function fish_prompt
+  set -l last_status $status
   set -l current_bg
 
   set -l set_normal (set_color normal)
@@ -77,6 +144,8 @@ function fish_prompt
   if not set -q VIRTUAL_ENV_DISABLE_PROMPT
     set -g VIRTUAL_ENV_DISABLE_PROMPT true
   end
+
+  __bobthefish_prompt_status $last_status
   echo -ns $set_tomato (whoami)
   # echo -ns $set_blue ' @ '
   # echo -ns $set_green (prompt_hostname)
