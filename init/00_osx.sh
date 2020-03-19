14685023387b40fc82461324ea0a98588ee1c8d0
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2020-03-19 08:59:51 +0000

    fix osx set up errors, use fish shell, update brew packages

--- init/00_osx.sh
+++ init/00_osx.sh
@@ -24,12 +24,6 @@ if [[ "$(type -P brew)" ]]; then
     brew doctor
     brew update
 
-    brew install wget --enable-iri
-    brew install vim --override-system-vi
-    brew tap homebrew/cask
-
-    brew install cask
-
     casks=(
         airflow
         android-file-transfer
@@ -42,18 +36,16 @@ if [[ "$(type -P brew)" ]]; then
         dbeaver-community
         discord
         docker
-        firefox
         firefox-developer-edition
         franz
-        google-chrome
         google-backup-and-sync
+        google-chrome
         grammarly
         iina
         iterm2
         java
         karabiner-elements
         lulu
-        now
         postman
         qbittorrent
         qlcolorcode
@@ -76,16 +68,12 @@ if [[ "$(type -P brew)" ]]; then
         yed
     )
 
-    brew tap caskroom/versions
-
     cask_list="$(to_install "${casks[*]}" "$(brew cask list 2>/dev/null)")"
     if [[ "$cask_list" ]]; then
         e_header "Installing Homebrew casks: $cask_list"
         brew cask install $cask_list
     fi
     brew cask cleanup
-
-    # ln -s "/opt/homebrew-cask/Caskroom/google-chrome/latest/Google Chrome.app/Contents/MacOS/Google\ Chrome" "/Users/tomatao/.bin/google-chome"
 
     # Install Homebrew recipes.
     recipes=(
@@ -95,22 +83,24 @@ if [[ "$(type -P brew)" ]]; then
         curl
         ffmpeg
         fish
+        gh
         git
+        gnutls
         heroku
         highlight
         imagemagick
         jq
         mongodb
-        mycli
         mysql
         nginx
-        pgcli
         postgresql
+        shellcheck
         sqlite
         ssh-copy-id
         tldr
         tree
         vim
+        wget
     )
 
     recipe_list="$(to_install "${recipes[*]}" "$(brew list)")"
@@ -123,12 +113,19 @@ if [[ "$(type -P brew)" ]]; then
     local binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
 
     # bash
-    if [[ "$( type -P $binroot/bash)" && ! "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
+    if [[ "$(type -P $binroot/bash)" && ! "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
         e_header "Adding $binroot/bash to the list of acceptable shells"
         echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
     fi
+
+    # fish
+    if [[ "$(type -P $binroot/fish)" && ! "$(cat /etc/shells | grep -q "$binroot/fish")" ]]; then
+        e_header "Adding $binroot/fish to the list of acceptable shells"
+        echo "$binroot/fish" | sudo tee -a /etc/shells >/dev/null
+    fi
+
     if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
-        e_header "Making $binroot/bash your default sheman ll"
+        e_header "Making $binroot/bash your default shell"
         sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
     fi
 fi
