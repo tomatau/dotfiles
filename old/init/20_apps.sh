43ca4b7e5dfa46ae5a58be03650e7aabc8e68256
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2025-09-13 19:31:22 +0100

    remove old subl setup

--- old/init/20_apps.sh
+++ old/init/20_apps.sh
@@ -6,28 +6,6 @@
 declare app_settings_path="$PARENT/app-settings"
 declare app_support_path="$HOME/Library/Application Support"
 
-# Sublime Text
-
-e_header "Copying Sublime Text PackageControl"
-
-local sublime_installed_packages="$app_support_path/Sublime Text/Installed Packages/"
-
-if [[ ! -d "$sublime_installed_packages" ]]; then
-    mkdir -p "$sublime_installed_packages"
-fi
-cp -r "$app_settings_path/subl/Installed Packages/" "$sublime_installed_packages"
-
-e_header "Copying Sublime Text User Settings"
-
-local sublime_user_packages="$app_support_path/Sublime Text/Packages/User/"
-
-if [[ ! -d "$sublime_user_packages" ]]; then
-    mkdir -p "$sublime_user_packages"
-fi
-
-cp -r "$app_settings_path/subl/User/" "$sublime_user_packages"
-
-
 # TextMate
 
 e_header "Install TextMate Settings"
